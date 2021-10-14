46da5084ae66e89f379603095dddacea18d77f56
Adolf <owner@Users-MacBook-Air.local>
2021-10-13 19:30:57 -0700

    queried data in SongList component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
     return <div>SongList</div>;
   }
 }
 
-export default SongList;
+// Define the query
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+// Bond the query and the component together:
+// This is what actually executes the query when our SongList component gets rendered to the screen.
+export default graphql(query)(SongList);
