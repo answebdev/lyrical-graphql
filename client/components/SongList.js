d29e3178a210a4d69f03c8d65d6f68916123aea0
Adolf <owner@Users-MacBook-Air.local>
2021-10-15 13:41:31 -0700

    add delete mutation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -5,11 +5,22 @@ import { graphql } from 'react-apollo';
 import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    // Call mutation and pass along the ID as a query variable to that mutation -
+    // define the query variables that we want to pass along
+    this.props.mutate({ variables: { id: id } });
+
+    // Refetch query after mutation runs (see notes in 'SongCreate' regarding 'refetchQueries'):
+  }
+
   renderSongs() {
-    return this.props.data.songs.map((song) => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className='collection-item'>
-          {song.title}
+        <li key={id} className='collection-item'>
+          {title}
+          <i className='material-icons' onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
         </li>
       );
     });
@@ -42,6 +53,14 @@ class SongList extends Component {
 //   }
 // `;
 
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
 // Bond the query and the component together:
 // This is what actually executes the query when our SongList component gets rendered to the screen.
-export default graphql(query)(SongList);
+export default graphql(mutation)(graphql(query)(SongList));
