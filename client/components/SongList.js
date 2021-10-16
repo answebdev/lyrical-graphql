c30fbb9d420ded40316d134172e6a79521d41734
Adolf <owner@Users-MacBook-Air.local>
2021-10-15 18:04:33 -0700

    add logic to refetch query after deleting song

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -8,11 +8,31 @@ class SongList extends Component {
   onSongDelete(id) {
     // Call mutation and pass along the ID as a query variable to that mutation -
     // define the query variables that we want to pass along
-    this.props.mutate({ variables: { id: id } });
+    this.props
+      .mutate({ variables: { id: id } })
+      // Refetch query after mutation runs (see notes in 'SongCreate' regarding 'refetchQueries').
+      // Refetch list of songs from the backend - a different method than the 'refetchQueries' way of doing it => using the 'refetch' function.
+      // The 'refetch' function will automatically re-execute any queries that are associated with the SongList component.
+      // In this case, this component has exactly one query associated with it (the 'fetchSongs' query).
+      // So we want to make sure that whenever we delete a song, it refetches the data that is associated with this 'fetchSongs' query.
 
-    // Refetch query after mutation runs (see notes in 'SongCreate' regarding 'refetchQueries'):
+      // 'refetch' vs' 'refetchQueries' (as in 'SongCreate.js').
+      // It really depends on how you are trying to update your query associated with which component in your hierarchy.
+      // In the SongCreate component, we want to update a query that is NOT associated with the SongCreate component -
+      // it's associated with a totally different component in our application.
+      // So we can not use 'this.props.data.refetch()' because the query that we want to refetch is not associated with the SongCreate component -
+      // SongCreate has no idea that the query (the 'fetchSong' query) exists, as being tied with the SongCreate component.
+
+      // However, the SongList component does receive that query inside the props ('this.props.data') object.
+      // So since it came in as a prop, GraphQL knows that that query exists, and knows that it is associated with SongList,
+      // and so it's going to modify the 'refetch' function (see below) to make sure that whenever we call it,
+      // it will refetch the query that is associated with the component.
+
+      // Using 'refetch' is the most simple, most straightforward way of rerunning a query.
+      // It forces the query to rerun on the server, and so it's going to blow away any existing fetch data we have to rerender our component.
+      // IN GENERAL: If you want to update the data associated with a particular component, just use 'refetch'.
+      .then(() => this.props.data.refetch());
   }
-
   renderSongs() {
     return this.props.data.songs.map(({ id, title }) => {
       return (
