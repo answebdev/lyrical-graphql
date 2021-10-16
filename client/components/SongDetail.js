b772ce6e802a88347fc963874f69b5f96cb6067d
Adolf <owner@Users-MacBook-Air.local>
2021-10-15 19:03:01 -0700

    display song title

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -4,11 +4,20 @@ import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
-    console.log(this.props);
+    // console.log(this.props);
+
+    // Destructure
+    const { song } = this.props.data;
+
+    // If there is no song yet (i.e., the query is still pending), show "Loading..." (could also show a spinner here - or you can choose to not show anything =>
+    // it might be more annoying for a user to see "Loading..." flash for a split second):
+    if (!song) {
+      return <div>Loading...</div>;
+    }
 
     return (
       <div>
-        <h3>Song Detail</h3>
+        <h3>{song.title}</h3>
       </div>
     );
   }
