3c4cecf8214f9db096925b9d77ca5590d2d576f2
Adolf <owner@Users-MacBook-Air.local>
2021-10-15 19:08:33 -0700

    add navigation

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import { graphql } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
 
@@ -17,6 +18,7 @@ class SongDetail extends Component {
 
     return (
       <div>
+        <Link to='/'>Back</Link>
         <h3>{song.title}</h3>
       </div>
     );
