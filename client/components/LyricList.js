2666f64a03e6d94f3a0b6c9125cde16d2b5b62b1
Adolf <owner@Users-MacBook-Air.local>
2021-10-17 10:29:19 -0700

    render lyrics on page

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -12,7 +12,7 @@ class LyricList extends Component {
   }
 
   render() {
-    return <ul className='collection'>Lyric List</ul>;
+    return <ul className='collection'>{this.renderLyrics()}</ul>;
   }
 }
 
