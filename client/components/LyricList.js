10cf42cb654e5be8cbc32904663b492269bb93cd
Adolf <owner@Users-MacBook-Air.local>
2021-10-17 18:18:07 -0700

    render number of likes on page

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
   onLike(id) {
-    console.log(id);
+    // console.log(id);
+    // Call the mutation
+    this.props.mutate({
+      variables: { id: id },
+    });
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className='collection-item'>
           {content}
           <i className='material-icons' onClick={() => this.onLike(id)}>
             thumb_up
           </i>
+          {likes}
         </li>
       );
     });
@@ -32,4 +39,4 @@ const mutation = gql`
   }
 `;
 
-export default LyricList;
+export default graphql(mutation)(LyricList);
