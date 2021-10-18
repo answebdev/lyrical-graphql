ea2040eb7f9d779abd4c9f388e45ccc46a88a96c
Adolf <owner@Users-MacBook-Air.local>
2021-10-17 18:55:53 -0700

    add optimistic response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,11 +3,28 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
-  onLike(id) {
+  onLike(id, likes) {
     // console.log(id);
     // Call the mutation
     this.props.mutate({
       variables: { id: id },
+      // Tell Apollo to use an optimistic response -
+      // When this mutation runs, try to use this data as soon as possible:
+      optimisticResponse: {
+        // We need to specifically say that we are making a Mutation:
+        __type: 'Mutation',
+        // Provide the response that we expect to see from our backend server:
+        likeLyric: {
+          id: id,
+          // This information comes from the Network tab in the console.
+          __typename: 'LyricType',
+          // This is the optimistic part of the response -
+          // we are guessing that the request is going to go through successfully,
+          // and we are hoping that when the response comes back, there's going to be an increment value
+          // for the number of likes:
+          likes: likes + 1,
+        },
+      },
     });
   }
 
@@ -17,7 +34,10 @@ class LyricList extends Component {
         <li key={id} className='collection-item'>
           {content}
           <div className='vote-box'>
-            <i className='material-icons' onClick={() => this.onLike(id)}>
+            <i
+              className='material-icons'
+              onClick={() => this.onLike(id, likes)}
+            >
               thumb_up
             </i>
             {likes}
